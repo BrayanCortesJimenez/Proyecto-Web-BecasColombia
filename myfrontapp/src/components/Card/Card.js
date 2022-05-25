31c6b7d6022d10539107c4b995d05196f146bd78
BrayanCortesJimenez <cortes.brayan@correounivalle.edu.co>
2022-05-24 21:51:46 -0500

    agragando link de noticias a la imagen

--- myfrontapp/src/components/Card/Card.js
+++ myfrontapp/src/components/Card/Card.js
@@ -6,7 +6,9 @@ function NewsCard({news}){
     return(
        <li className="newsCard">
         <a className="title" href={news.url}>{news.title}</a>
-        <img className="newsImage" src ={news.multimedia[2].url} alt={news.title}/>
+        <a href={news.url}>
+            <img className="newsImage" src ={news.multimedia[2].url} alt={news.title}/>
+        </a>
         <p className="Abstract">{news.abstract}</p>
         </li> 
     );
